cbfafbf6b283adb2058aa1e5b011c95936774db4
apharrison964 <apharrison964@gmail.com>
2015-11-27 19:55:04 -0500

    Modified the JS, added the script to the top of the file

--- html/script.js
+++ html/script.js
@@ -2,8 +2,11 @@ function insertAfter( referenceNode, newNode ) {
     referenceNode.parentNode.insertBefore( newNode, referenceNode.nextSibling );
 }
 
-var table = document.createElement("TABLE");
-table.border = "1";
-document.body.appendChild(table);
-var getTeamForm = document.getElementById("teamForm");
-insertAfter(getTeamForm, createTable);
+function makeTable() {
+	var table = document.createElement("TABLE");
+	table.border = "1";
+	document.body.appendChild(table);
+	var getTeamForm = document.getElementById("teamForm");
+	insertAfter(getTeamForm, createTable);
+}
+
