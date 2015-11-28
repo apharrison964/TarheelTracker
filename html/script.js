acc50ca5a181997d8f75ea1ba253a58f4c399096
Alexandra Harrison <apharri3@live.unc.edu>
2015-11-27 19:30:16 -0500

    Added insertAfter function

--- html/script.js
+++ html/script.js
@@ -1,1 +1,7 @@
+function insertAfter( referenceNode, newNode ) {
+    referenceNode.parentNode.insertBefore( newNode, referenceNode.nextSibling );
+}
+
 var x = document.createElement("TABLE");
+// TODO: Add object for table to be created under (somewhere in the carousel)
+insertAfter(TODO , x);
