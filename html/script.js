7b637980bbdc95a65eceb6d0277662867e026174
Alexandra Harrison <apharri3@live.unc.edu>
2015-11-27 19:46:18 -0500

    Added some stuff

--- html/script.js
+++ html/script.js
@@ -2,6 +2,8 @@ function insertAfter( referenceNode, newNode ) {
     referenceNode.parentNode.insertBefore( newNode, referenceNode.nextSibling );
 }
 
-var createTable = document.createElement("TABLE");
+var table = document.createElement("TABLE");
+table.border = "1";
+document.body.appendChild(table);
 var getTeamForm = document.getElementById("teamForm");
 insertAfter(getTeamForm, createTable);
