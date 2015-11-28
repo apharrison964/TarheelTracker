7a51900d3fd18d17ed893f6ef2c4208b04f0fd57
Alexandra Harrison <apharri3@live.unc.edu>
2015-11-27 19:40:36 -0500

    Added ID for the form, changed variables

--- html/script.js
+++ html/script.js
@@ -2,7 +2,6 @@ function insertAfter( referenceNode, newNode ) {
     referenceNode.parentNode.insertBefore( newNode, referenceNode.nextSibling );
 }
 
-var x = document.createElement("TABLE");
-var y = document.getElementById()
-// TODO: Add object for table to be created under (somewhere in the carousel)
-insertAfter(TODO , x);
+var createTable = document.createElement("TABLE");
+var getTeamForm = document.getElementById("teamForm");
+insertAfter(getTeamForm, createTable);
