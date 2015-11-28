31130a65bd05da92c9dca994ae394ac01cd73db5
Alexandra Harrison <apharri3@live.unc.edu>
2015-11-27 20:13:24 -0500

    Added row and columns

--- html/script.js
+++ html/script.js
@@ -4,8 +4,18 @@ function insertAfter( referenceNode, newNode ) {
 
 function makeTable() {
 	var table = document.createElement("TABLE");
+	table.setAttribute("id", "createdTable");
 	table.border = "1";
 	document.body.appendChild(table);
+	
+	var row = document.createElement("TR");
+	row.setAttribute("id", "row");
+	document.getElementById("createdTable").appendChild(row);
+	
+	var column = document.createElement("TD");
+	column.setAttribute("id", "column");
+	document.getElementById("createdTable").appendChild(column);
+	
 	var getTeamForm = document.getElementById("teamForm");
 	insertAfter(getTeamForm, createTable);
 }
