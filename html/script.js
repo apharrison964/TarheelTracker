afa2304cc9ad7da47dfb15dbe413670733955046
apharrison964 <apharrison964@gmail.com>
2015-11-30 12:37:33 -0500

    That worked trying something else

--- html/script.js
+++ html/script.js
@@ -1,25 +1,24 @@
 // This function may not be working right
-// function insertAfter(referenceNode, newNode) {
-    // referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-// }
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
 
 // TODO: function not working, pressing submit just resets the page
 function makeTable() {
-	alert("Submit button pressed");
-	// var table = document.createElement("TABLE");
-	// table.setAttribute("id", "createdTable");
-	// table.border = "1";
-	// document.body.appendChild(table);
-// 	
-	// var row = document.createElement("TR");
-	// row.setAttribute("id", "row");
-	// document.getElementById("createdTable").appendChild(row);
-// 	
-	// var column = document.createElement("TD");
-	// column.setAttribute("id", "column");
-	// document.getElementById("createdTable").appendChild(column);
-// 	
-	// var getTeamForm = document.getElementById("teamForm");
-	// insertAfter(getTeamForm, createTable);
+	var table = document.createElement("TABLE");
+	table.setAttribute("id", "createdTable");
+	table.border = "1";
+	document.body.appendChild(table);
+	alert("It reached where it is about to create the row");
+	var row = document.createElement("TR");
+	row.setAttribute("id", "row");
+	document.getElementById("createdTable").appendChild(row);
+	
+	var column = document.createElement("TD");
+	column.setAttribute("id", "column");
+	document.getElementById("createdTable").appendChild(column);
+	
+	var getTeamForm = document.getElementById("teamForm");
+	insertAfter(getTeamForm, createTable);
 }
 
