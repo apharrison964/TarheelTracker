6dfe872508cf8314e21c9cba0ed2cefab799855d
Alexandra Harrison <apharri3@live.unc.edu>
2015-12-09 15:32:21 -0500

    Update BasketballView.js

--- clientSide/BasketballView.js
+++ clientSide/BasketballView.js
@@ -18,49 +18,21 @@ $("#playerTable").html("<table border='1'><tr><td>First Name</td><td>Last Name</
 	$('#teamForm').on('submit',
 			       function (e) {
 				   e.preventDefault();
-				   $.ajax(url_base + "/PlayerRun.php",
-					  {type: "POST",
-						  dataType: "json",
-						  data: $(this).serialize(),
-						  success: function(todo_json, status, jqXHR) {
-						  var p = new Player(todo_json);
-						  $('#todo_list').append(p.makeCompactDiv());
-					      },
-						  error: function(jqXHR, status, error) {
-						  alert(jqXHR.responseText);
 					      }});
 			       });
 
 
-	$('#playerForm').on('submit',
-			   'form.edit_form',
-			   null,
-			   function (e) {
-			       e.preventDefault();
-			       var edit_div = $(this).parent();
-			       var p = edit_div.data('todo');
-			       $.ajax(url_base + "/PlayerRun.php/" + p.playerID,
-				      {type: "POST",
-					      dataType: "json",
-					      data: $(this).serialize(),
-					      success: function(todo_json, status, jqXHR) {
-					      var p = new Player(todo_json);
-					      edit_div.replaceWith(p.makeCompactDiv());
-					  },
-					      error: function(jqXHR, status, error) {
-					      alert(jqXHR.responseText);
-					  }});
-			   });
+	
     });
 
 
 var loadPlayerItem = function (playerID) {
     $.ajax(url_base + "/PlayerRun.php/" + playerID,
-{type: "GET",
- dataType: "json",
- success: function(player_json, status, jqXHR) {
-	var p = new Player(player_json);
-	$('#playerTable').append(p.makeCompactDiv());
-    }
+	{type: "GET",
+	 dataType: "json",
+	 success: function(player_json, status, jqXHR) {
+		var p = new Player(player_json);
+		$('#playerTable').append(p.makeCompactDiv());
+	    }
 });
 }
