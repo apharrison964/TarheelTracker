c0379ba17427654489e28a7c070540bae88f7c2e
Alexandra Harrison <apharri3@live.unc.edu>
2015-12-10 01:36:35 -0500

    Did it!!

--- clientSide/BasketballView.js
+++ clientSide/BasketballView.js
@@ -1,7 +1,7 @@
 var url_base = "http://wwwp.cs.unc.edu/Courses/comp426-f15/users/apharri3/Codiad/workspace/finalProject/serverSide";
 
 $(document).ready(function () {
-$("#playerTable").html("<table border='1'><tr><td>First Name</td><td>Last Name</td><td>Player ID</td></tr><tr><td>Shaquille</td><td>O'Neil</td></tr><tr><td>Kareem</td><td>Abdul-Jabar</td></tr><tr><td>Dirk</td><td>Nowitzki</td></tr><tr><td>Michael</td><td>Jordan</td></tr></table>");
+$("#playerTable").html("<table border='1'><tr><td>First Name</td><td>Last Name</td><td>Player ID</td></tr><tr><td>Kareem</td><td>Abdul-Jabbar</td><td>1</td></tr><tr><td>Michael</td><td>Jordan</td><td>2</td></tr><tr><td>Dirk</td><td>Nowitzki</td><td>3</td></tr><tr><td>Shaquille</td><td>O'Neal</td><td>4</td></tr></table>");
 //$("#playerTable").html("<table class="table"> <thead> <tr> <th>First Name</th> <th>Last Name</th> <th>Lastname</th> </tr></thead> <tbody> <tr> <td>Shaquille</td><td>O'Neil</td></tr><tr> <td>Kareem</td><td>Abdul-Jabar</td></tr><tr> <td>Dirk</td><td>Nowitzki</td></tr><tr> <td>Michael</td><td>Jordan</td></tr></tbody> </table>");
 
 	$.ajax(url_base + "/PlayerRun.php",
