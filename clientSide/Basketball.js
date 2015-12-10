4c60e438b52a484d316e9f8a1de0f5f510dc3b8d
Alexandra Harrison <apharri3@live.unc.edu>
2015-12-10 01:41:15 -0500

    Got rid of a function

--- clientSide/Basketball.js
+++ clientSide/Basketball.js
@@ -1,4 +1,4 @@
-console.log("TEST");
+
 var Player = function(player_json) {
     this.firstName = player_json.firstName;
     this.lastName = player_json.lastName;
@@ -31,46 +31,3 @@ Player.prototype.makeCompactDiv = function() {
 
     return contentDiv;
 };
-
-Player.prototype.makeEditDiv = function() {
-    var ediv = $("<div></div>");
-
-    var ediv_form = $("<form></form>");
-    ediv_form.addClass('edit_form');
-    
-    ediv_form.append($("<input type='text' name='title'>").val(this.title));
-    ediv_form.append("<br>");
-
-    ediv_form.append($("<textarea name='note'></textarea>").val(this.note));
-    ediv_form.append("<br>");
-
-    ediv_form.append("Project: ");
-    ediv_form.append($("<input type='text' name='project'>").val(this.project));
-    ediv_form.append("<br>");
-
-    ediv_form.append("Due Date: ");
-    if (this.due_date) {
-	ediv_form.append($("<input type='text' name='due_date'>").val(this.due_date.toDateString()));
-    } else {
-	ediv_form.append($("<input type='text' name='due_date'>"));
-    }
-    ediv_form.append("<br>");
-
-    ediv_form.append("Priority: ");
-    ediv_form.append($("<input type='text' name='priority'>").val(this.priority));
-    ediv_form.append("<br>");
-
-    ediv_form.append("Complete ");
-    complete_checkbox = $("<input type='checkbox' name='complete' value=1>");
-    if (this.complete) {
-	complete_checkbox[0].checked = true;
-    }
-    ediv_form.append(complete_checkbox);
-    ediv_form.append("<br>");
-
-    ediv_form.append("<button type='submit'>Save</button><button type='button' class='cancel'>Cancel</button>");
-    ediv.append(ediv_form);
-    
-    ediv.data('player', this);
-    return ediv;
-}
